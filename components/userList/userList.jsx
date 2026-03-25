c1683b391f0ee5fcbb3fb5886edd26cb771bf7c6
ColePeterson05 <cpeter70@charlotte.edu>
2026-03-25 15:30:49 -0400

    Add compiled bundle and update UserList
    Add webpack-generated compiled/photoShare.bundle.js. Update UserList component to fetch /user/list in componentDidMount and add loading/error state handling; update userList.css accordingly. Also include minor changes to photoShare.jsx (entry) to reflect the component updates.

--- components/userList/userList.jsx
+++ components/userList/userList.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import {
   Divider,
   List,
@@ -15,33 +16,68 @@ import './userList.css';
 class UserList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      users: [],
+      loading: true,
+      error: null,
+    };
+  }
+
+  componentDidMount() {
+    // Fetch user list from the server
+    fetch('/user/list')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({
+          users: data,
+          loading: false,
+        });
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+        this.setState({
+          error: error.message,
+          loading: false,
+        });
+      });
   }
 
   render() {
+    const { users, loading, error } = this.state;
+
+    if (loading) {
+      return <Typography variant="body2">Loading users...</Typography>;
+    }
+
+    if (error) {
+      return <Typography variant="body2" color="error">Error loading users: {error}</Typography>;
+    }
+
     return (
       <div>
-        <Typography variant="body1">
-          This is the user list, which takes up 3/12 of the window.
-          You might choose to use <a href="https://mui.com/components/lists/">Lists</a> and <a href="https://mui.com/components/dividers/">Dividers</a> to
-          display your users like so:
+        <Typography variant="h6" style={{ marginBottom: '16px' }}>
+          Users
         </Typography>
         <List component="nav">
-          <ListItem>
-            <ListItemText primary="Item #1" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #2" />
-          </ListItem>
-          <Divider />
-          <ListItem>
-            <ListItemText primary="Item #3" />
-          </ListItem>
-          <Divider />
+          {users.map((user, index) => (
+            <div key={user._id}>
+              <Link to={`/users/${user._id}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <ListItem sx={{ '&:hover': { backgroundColor: '#f5f5f5' }, cursor: 'pointer' }}>
+                  <ListItemText
+                    primary={`${user.first_name} ${user.last_name}`}
+                    secondary={user.occupation}
+                  />
+                </ListItem>
+              </Link>
+              {index < users.length - 1 && <Divider />}
+            </div>
+          ))}
         </List>
-        <Typography variant="body1">
-          The model comes in from window.models.userListModel()
-        </Typography>
       </div>
     );
   }
--- photoShare.jsx
+++ photoShare.jsx
@@ -22,44 +22,42 @@ class PhotoShare extends React.Component {
   render() {
     return (
       <HashRouter>
-      <div>
-      <Grid container spacing={8}>
-        <Grid item xs={12}>
-          <TopBar/>
-        </Grid>
-        <div className="main-topbar-buffer"/>
-        <Grid item sm={3}>
-          <Paper className="main-grid-item">
-            <UserList />
-          </Paper>
-        </Grid>
-        <Grid item sm={9}>
-          <Paper className="main-grid-item">
-            <Switch>
-            <Route exact path="/"
-                render={() => (
-                <Typography variant="body1">
-                  Welcome to your photosharing app! This <a href="https://mui.com/components/paper/">Paper</a> component
-                  displays the main content of the application. The {"sm={9}"} prop in
-                  the <a href="https://mui.com/components/grid/">Grid</a> item component makes it responsively
-                  display 9/12 of the window. The Switch component enables us to conditionally render different
-                  components to this part of the screen. You don&apos;t need to display anything here on the homepage,
-                  so you should delete this Route component once you get started.
-                </Typography>
-                )}
-              />
-              <Route path="/users/:userId"
-                render={ props => <UserDetail {...props} /> }
-              />
-              <Route path="/photos/:userId"
-                render ={ props => <UserPhotos {...props} /> }
-              />
-              <Route path="/users" component={UserList}  />
-            </Switch>
-          </Paper>
-        </Grid>
-      </Grid>
-      </div>
+        <div>
+          <TopBar />
+          <div className="main-topbar-buffer" />
+          <Grid container spacing={2} style={{ padding: '16px' }}>
+            <Grid item xs={12} sm={3}>
+              <Paper className="main-grid-item">
+                <UserList />
+              </Paper>
+            </Grid>
+            <Grid item xs={12} sm={9}>
+              <Paper className="main-grid-item">
+                <Switch>
+                  <Route exact path="/"
+                    render={() => (
+                      <Typography variant="body1">
+                        Welcome to your photosharing app! This <a href="https://mui.com/components/paper/">Paper</a> component
+                        displays the main content of the application. The {"sm={9}"} prop in
+                        the <a href="https://mui.com/components/grid/">Grid</a> item component makes it responsively
+                        display 9/12 of the window. The Switch component enables us to conditionally render different
+                        components to this part of the screen. You don&apos;t need to display anything here on the homepage,
+                        so you should delete this Route component once you get started.
+                      </Typography>
+                    )}
+                  />
+                  <Route path="/users/:userId"
+                    render={ props => <UserDetail {...props} /> }
+                  />
+                  <Route path="/photos/:userId"
+                    render ={ props => <UserPhotos {...props} /> }
+                  />
+                  <Route path="/users" component={UserList}  />
+                </Switch>
+              </Paper>
+            </Grid>
+          </Grid>
+        </div>
       </HashRouter>
     );
   }
