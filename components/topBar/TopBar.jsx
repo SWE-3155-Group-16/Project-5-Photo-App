d9d9f5075c3c758da330373a16760a38bdcc229e
FoggierSummer23 <harrisonsyfert@gmail.com>
2026-03-25 19:53:52 -0400

    TopBar + UserDetails

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -5,20 +5,122 @@ import {
 import './TopBar.css';
 
 /**
- * Define TopBar, a React componment of project #5
+ * Define TopBar, a React component of project #5
  */
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      version: '',
+      user: null
+    };
+  }
+
+  componentDidMount() {
+    this.loadVersion();
+    this.loadUserIfNeeded();
+  }
+
+  componentDidUpdate(prevProps) {
+    const oldUserId = prevProps.match?.params?.userId;
+    const newUserId = this.props.match?.params?.userId;
+
+    if (oldUserId !== newUserId) {
+      this.loadUserIfNeeded();
+    }
+
+    const oldPath = prevProps.location?.pathname;
+    const newPath = this.props.location?.pathname;
+
+    if (oldPath !== newPath) {
+      this.loadUserIfNeeded();
+    }
+  }
+
+  loadVersion() {
+    fetch('/test/info')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch version info');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ version: data.__v || '' });
+      })
+      .catch(() => {
+        this.setState({ version: '' });
+      });
+  }
+
+  loadUserIfNeeded() {
+    const userId = this.props.match?.params?.userId;
+
+    if (!userId) {
+      this.setState({ user: null });
+      return;
+    }
+
+    fetch(`/user/${userId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch user');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ user: data });
+      })
+      .catch(() => {
+        this.setState({ user: null });
+      });
+  }
+
+  getContextText() {
+    const { user } = this.state;
+    const pathname = this.props.location?.pathname || '';
+
+    if (!user) {
+      return '';
+    }
+
+    const fullName = `${user.first_name} ${user.last_name}`;
+
+    if (pathname.startsWith('/photos/')) {
+      return `Photos of ${fullName}`;
+    }
+
+    if (pathname.startsWith('/users/')) {
+      return fullName;
+    }
+
+    return '';
   }
 
   render() {
+    const { version } = this.state;
+    const contextText = this.getContextText();
+
     return (
       <AppBar className="topbar-appBar" position="absolute">
-        <Toolbar>
-          <Typography variant="h5" color="inherit">
-              This is the TopBar component
+        <Toolbar className="topbar-toolbar">
+          <Typography variant="h6" color="inherit">
+            PhotoShare Team
           </Typography>
+
+          <div className="topbar-right">
+            {contextText && (
+              <Typography variant="h6" color="inherit">
+                {contextText}
+              </Typography>
+            )}
+
+            {version && (
+              <Typography variant="body1" color="inherit">
+                Version: {version}
+              </Typography>
+            )}
+          </div>
         </Toolbar>
       </AppBar>
     );
--- components/userDetail/userDetail.jsx
+++ components/userDetail/userDetail.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {
-  Typography
+  Typography,
+  Button
 } from '@mui/material';
-import './userDetail.css';
-
+import { Link } from 'react-router-dom';
+import './userDetails.css';
 
 /**
  * Define UserDetail, a React component of project #5
@@ -11,17 +12,94 @@ import './userDetail.css';
 class UserDetail extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      user: null,
+      error: null
+    };
+  }
+
+  componentDidMount() {
+    this.loadUser();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.match.params.userId !== this.props.match.params.userId) {
+      this.loadUser();
+    }
+  }
+
+  loadUser() {
+    const userId = this.props.match.params.userId;
+
+    fetch(`/user/${userId}`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Failed to fetch user details');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        this.setState({ user: data, error: null });
+      })
+      .catch((error) => {
+        this.setState({ error: error.message, user: null });
+      });
   }
 
   render() {
+    const { user, error } = this.state;
+
+    if (error) {
+      return (
+        <Typography variant="body1" color="error">
+          {error}
+        </Typography>
+      );
+    }
+
+    if (!user) {
+      return (
+        <Typography variant="body1">
+          Loading user details...
+        </Typography>
+      );
+    }
+
     return (
-      <Typography variant="body1">
-        This should be the UserDetail view of the PhotoShare app. Since
-        it is invoked from React Router the params from the route will be
-        in property match. So this should show details of user:
-        {this.props.match.params.userId}. You can fetch the model for the
-        user from window.models.userModel(userId).
-      </Typography>
+      <div className="user-detail-container">
+        <Typography variant="h4" gutterBottom>
+          {user.first_name} {user.last_name}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>First name:</strong> {user.first_name}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>Last name:</strong> {user.last_name}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>Location:</strong> {user.location}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>Description:</strong> {user.description}
+        </Typography>
+
+        <Typography variant="body1" gutterBottom>
+          <strong>Occupation:</strong> {user.occupation}
+        </Typography>
+
+        <Button
+          component={Link}
+          to={`/photos/${user._id}`}
+          variant="contained"
+          sx={{ mt: 2 }}
+        >
+          View Photos
+        </Button>
+      </div>
     );
   }
 }
