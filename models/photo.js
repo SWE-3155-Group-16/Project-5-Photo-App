ba4cf30d59e9444aa45ecf99b5338043fa23c130
William Brodersen <wbroders@charlotte.edu>
2026-05-04 20:35:48 -0400

    Update photo.js

--- models/photo.js
+++ models/photo.js
@@ -12,6 +12,15 @@ const photoSchema = new mongoose.Schema({
   file_name: { type: String, required: true },
   date_time: { type: Date, default: Date.now },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+
+  // undefined/missing = public
+  // [] = owner only
+  // [userIds] = owner + listed users
+  sharing_list: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+
   comments: [commentSchema],
 });
 
